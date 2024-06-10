f0f8ec344a65de73d3756d7a562aa476eb01ed3e
TT-thzy <shy11cat@outlook.com>
2024-06-10 22:30:48 +0800

    feat-microservice: gateway使用学习

---
+++ SpringCloud/Gateway/api-gateway/src/main/java/com/thzy/gateway/GateWayApplication.java
@@ -0,0 +1,18 @@
+package com.thzy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: MicroServiceStudy
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2024-06-10 17:52
+ **/
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
